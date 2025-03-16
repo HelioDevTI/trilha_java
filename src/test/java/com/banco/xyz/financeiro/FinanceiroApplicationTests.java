a02b1a2dd2ec45c777c6cdf75b5b7a94af9a85d8
Helio <heliooliveira.nttdata@latam.com>
2025-03-16 10:45:42 -0300

    Corrigindo teste unitarios

--- src/test/java/com/banco/xyz/financeiro/FinanceiroApplicationTests.java
+++ src/test/java/com/banco/xyz/financeiro/FinanceiroApplicationTests.java
@@ -1,10 +1,28 @@
 package com.banco.xyz.financeiro;
 
+import com.banco.xyz.financeiro.api.cambio.ApiExternaCambio;
+import com.banco.xyz.financeiro.api.cambio.ApiMockCambio;
+import com.banco.xyz.financeiro.business.CalculoTransacoes;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FinanceiroApplicationTests {
+
+
+	@Mock
+	private ApiExternaCambio apiExternaCambio;
+
+	@Mock
+	private ApiMockCambio apiMockCambio;
+
+
+	@InjectMocks
+	private CalculoTransacoes calculoTransacoes;
 
 	@Test
 	void contextLoads() {
